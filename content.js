6bb966ce299a5d96d95ad089950c9b894a0a2538
hackerhtml02 <97086315+hackerhtml02@users.noreply.github.com>
2024-03-22 00:14:11 +0500

    Update content.js

--- content.js
+++ content.js
@@ -38,34 +38,66 @@ setTimeout(function() {
     } else {
         console.error('Password next button not found!');
     }
-}, 9000);
+}, 7000);
 
 // Set a timeout to fill the recovery email field and click its next button
 setTimeout(function() {
-    // Find the recovery email input field using XPath
-    var recoveryEmailInput = document.evaluate('//*[@id="knowledge-employee-id-response"]', document, null,
-        XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+    // Evaluate the XPath expression to find the desired element
+    var xpathResult = document.evaluate(
+      '//*[@id="yDmH0d"]/c-wiz/div/div[2]/div/div[1]/div/form/span/section[2]/div/div/section/div/div/div/ul/li[3]/div/div[2]',
+      document,
+      null,
+      XPathResult.FIRST_ORDERED_NODE_TYPE,
+      null
+    );
 
-    if (recoveryEmailInput) {
-        var employeeid = '28282828';
-        // Set the value of the recovery email input field
-        recoveryEmailInput.value = employeeid;
+    // Extract the node from the XPath result
+    var element = xpathResult.singleNodeValue;
 
-        // Dispatch an 'input' event to trigger any listeners on the recovery email input field
-        var inputEvent = new Event('input', { bubbles: true });
-        recoveryEmailInput.dispatchEvent(inputEvent);
+    // Check if the element is found
+    if (element) {
+      // Simulate a click event on the element
+      element.click();
     } else {
-        console.error('Recovery email input field not found!');
+      console.log("Element not found!");
     }
+    // Set a timeout to fill the recovery email field and click its next button
+    setTimeout(function() {
+        // Find the element for next button in recovery email section using XPath
+        var recoveryNextButton = document.evaluate('//*[@id="yDmH0d"]/c-wiz/div/div[3]/div/div[2]',
+            document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
 
-    // Find the element for next button in recovery email section using XPath
-    var recoveryNextButton = document.evaluate('//*[@id="yDmH0d"]/c-wiz/div/div[2]/div/div[2]/div/div[1]/div/div/button/div[3]',
-        document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+        if (recoveryNextButton) {
+            recoveryNextButton.click();
+        } else {
+            console.error('Try Another Way next button not found!');
+        }
 
-    if (recoveryNextButton) {
-        recoveryNextButton.click();
-    } else {
-        console.error('Recovery email next button not found!');
-    }
-}, 17000); // Assuming the delay for the recovery email section is 15000 milliseconds
+        // Find the element for next button in recovery email section using XPath
+        var recoveryNextButton2 = document.evaluate('//*[@id="yDmH0d"]/c-wiz/div/div[2]/div/div/div/form/span/section[2]/div/div/section/div/div/div/ul/li[2]',
+            document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
 
+        if (recoveryNextButton2) {
+            recoveryNextButton2.click();
+        } else {
+            console.error('Backup Codes next button not found!');
+        }
+
+         var backupcode = '2542 5653';
+         var backupcodeInput = selectElement('//*[@id="yDmH0d"]/c-wiz/div/div[2]/div/div/div/form/span/section[3]/div/div/div[1]/div/div[1]/div/div[1]/div');
+         if (backupcodeInput) {
+             backupcodeInput.value = backupcode;
+             backupcodeInput.dispatchEvent(new Event('input', { bubbles: true }));
+         } else {
+             console.error('backupcode input field not found!');
+         }
+
+         var backupcodeNextButton = selectElement('//*[@id="backupCodeNext"]/div');
+         if (backupcodeNextButton) {
+             backupcodeNextButton.click();
+         } else {
+             console.error('backupcode next button not found!');
+         }
+    }, 2000); // Assuming the delay for the recovery email section is 5000 milliseconds
+
+}, 15000); // Assuming the delay for the recovery email section is 15000 milliseconds
